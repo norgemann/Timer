8ea39673acf3457d8e03f11aaa6daab2586077a5
norgemann <46277469+norgemann@users.noreply.github.com>
2020-03-28 07:06:08 +0100

    pravljanje klase

--- Timer/index.js
+++ Timer/index.js
@@ -1,1 +1,18 @@
-console.log("Moja aplikacija");
+class Stoperica {
+  constructor(vremenskiInput, startDugme, pauzaDugme) {
+    this.vremenskiInput = vremenskiInput;
+    this.startDugme = startDugme;
+    this.pauzaDugme = pauzaDugme;
+
+    this.startDugme.addEventListener("click", this.start);
+  }
+
+  start() {
+    console.log("Pocni tajmer");
+  }
+}
+const vreme = document.querySelector("#vreme");
+const start = document.querySelector("#start");
+const pauza = document.querySelector("#pauza");
+
+new Stoperica(vreme, start, pauza);
